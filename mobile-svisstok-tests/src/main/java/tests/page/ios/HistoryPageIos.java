59b87d4f6218b668de2d5f88feb62ea4394e001d
AlekseiMordas <endy990@gmail.com>
2014-05-07 15:51:02 +0300

    Fix HistoryPageIos

--- mobile-svisstok-tests/src/main/java/tests/page/ios/HistoryPageIos.java
+++ mobile-svisstok-tests/src/main/java/tests/page/ios/HistoryPageIos.java
@@ -1,234 +1,176 @@
 package tests.page.ios;
 
 import java.awt.Rectangle;
+import java.util.List;
 
-import org.testng.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
 
+import runner.DeviceConfig;
+import runner.Devices;
 import tests.page.HistoryPage;
 
 import com.annotation.FindBy;
 import com.element.UIView;
+import com.ios.AppiumDriver;
 import com.mobile.driver.nativedriver.NativeDriver;
 import com.mobile.driver.page.PageFactory;
-import com.mobile.driver.wait.Sleeper;
 
-public class HistoryPageIos extends HistoryPage{
+public class HistoryPageIos extends HistoryPage {
 	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/text[1]")
-	private UIView header;
-	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[1]/link[1]",
-			ios7 = "//window[1]/scrollview[1]/webview[1]/link[1]")
-	private UIView filterButton;
-	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[3]",
-			ios7 = "Все звонки")
-	private UIView allCalls;
-	
-	@FindBy(locator = "Пропущенные звонки")
-	private UIView missed;
-	
-	@FindBy(locator = "Входящие звонки")
-	private UIView incoming;
-	
-	@FindBy(locator = "Исходящие звонки")
-	private UIView outgoing;
-	
-	@FindBy(locator = "Несостоявшиеся вызовы")
-	private UIView rejected;
-	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/text[2]")
-	private UIView editButton;
-	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[2]/link[3]/text[1]")
-	private UIView nameContact;
-	
-	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[1]/link[1]")
-	private UIView backFromAllContacts;
+	protected static final String DEVICE = DeviceConfig.getDevice();
 
 	public HistoryPageIos(NativeDriver driver) {
 		super(driver);
 	}
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]")
+	private UIView webview;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/text[4]")
+	private UIView timerCall;
+
+	@FindBy(locator = "Позвонить", ios7 = "//window[1]/scrollview[1]/webview[1]/link[4]")
+	private UIView callTab;
+
+	// @FindBy(locator = "")
+	// private UIView editButton;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[4]")
+	private UIView trashButton;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/text[2]",
+			ios7 = "//window[1]/scrollview[1]/webview[1]/link[3]")
+	private UIView arrowButton;
+
+	@FindBy(locator = FIRST_RESULT)
+	private UIView contact;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[2]/link[1]",
+			ios7 = "//window[1]/scrollview[1]/webview[1]/link[2]")
+	private UIView cancelCallButton;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/text[3]")
+	private UIView messageEmptyList;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[2]")
+	private UIView allContacts;
+
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[3]")
+	private UIView settingButton;
 	
-	public enum Headers {
-		ALL_CALLS("Все звонки"), MISSED("Пропущенные звонки"), INCOMING(
-				"Входящие звонки"), OUTGOING("Исходящие звонки"), REJECTED(
-				"Несостоявшиеся вызовы");
+	@FindBy(locator = "//window[1]/scrollview[1]/webview[1]/link[1]")
+	private UIView filterButton;
 
-		private final String text;
-
-		private Headers(final String text) {
-			this.text = text;
-		}
-
-		@Override
-		public String toString() {
-			return text;
-		}
-	};
-
-	
-	public HistoryPageIos clickAllContacts() {
-//		List<WebElement> list = ((AppiumDriver) driver).getDriver().findElements(By.name("Все звонки"));
-//		Point point=  list.get(list.size()-1).getLocation();
-		Rectangle point=  allCalls.getLocation();
-		allCalls.touchWithCoordinates(point.getX(), point.getY());
-		Sleeper.SYSTEM_SLEEPER.sleep(2000);
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
-
-
-	public HistoryPageIos clickMissedCalls() {
-		missed.touch();
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
-
-	
-	public HistoryPageIos clickIncomingCalls() {
-		incoming.touch();
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
-
-	
-	public HistoryPageIos clickOutcommingCalls() {
-		outgoing.touch();
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
-
-	
-	public HistoryPageIos clickRejectedCalls() {
-		rejected.touch();
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
+	private static final String FIRST_RESULT = "//window[1]/scrollview[1]/webview[1]/link";//link[3]
 
 	@Override
 	public void checkPage() {
 		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public HistoryPageIos openFilter() {
-		Rectangle point = filterButton.getLocation();
-		filterButton.touchWithCoordinates(point.getX(), point.getY());
-		return PageFactory.initElements(driver, HistoryPageIos.class);
-	}
 
-	
-	public void checkHistoryFilter() {
-		Assert.assertEquals(Headers.ALL_CALLS.toString(), clickAllContacts()
-				.getTitle());
-		openFilter();
-		Assert.assertTrue(clickMissedCalls().isExistElement(header));
-		openFilter();
-		Assert.assertTrue(clickIncomingCalls().isExistElement(header));
-		openFilter();
-		Assert.assertTrue(clickOutcommingCalls().isExistElement(header));
-		openFilter();
-		Assert.assertTrue(clickRejectedCalls().isExistElement(header));
 	}
-	
-	
-	public void checkVisibleShortName(){
-		//TODO: strange naming of contacts
-//		String name = DeviceConfig.getCaller();
-//		Assert.assertEquals(name, clickAllContacts()
-//				.getName(nameContact));
-//		openFilter();
-//		Assert.assertEquals(name, clickMissedCalls()
-//				.getName(nameContact));
-//		openFilter();
-//		Assert.assertEquals(name, clickIncomingCalls()
-//				.getName(nameContact));
-//		openFilter();
-//		Assert.assertEquals(name, clickOutcommingCalls()
-//				.getName(nameContact));
-//		openFilter();
-//		Assert.assertEquals(name, clickRejectedCalls()
-//				.getName(nameContact));
-	}
-	
-	private String getTitle() {
-		Sleeper.SYSTEM_SLEEPER.sleep(1000);
-		return header.getAttribute("label");
-	}
-	
-	private boolean isExistElement(UIView element){
-		return element.isExists();
-	}
-	
-	private String getName(UIView element){
-		return element.getAttribute("name");
-	}
-
 
 	@Override
 	public void clickFirstContact() {
-		// TODO Auto-generated method stub
-		
+		Rectangle point = webview.getLocation();
+		double y = 42;
+		webview.touchWithCoordinates(point.getX(), point.getY() + y);
 	}
-
 
 	@Override
-	public <T> T cancelCall() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	public HistoryPageIos cancelCall() {
+		switch (Devices.valueOf(DEVICE)) {
+		case IPHONE:
+			Rectangle point = cancelCallButton.getLocation();
+			 cancelCallButton.touchWithCoordinates(point.getX(), point.getY());
+			break;
+		case IOS7:
+			cancelCallButton.touch();
+			break;
 
+		default:
+			break;
+		}
+		return PageFactory.initElements(driver, HistoryPageIos.class);
+	}
 
 	@Override
 	public String getTimer() {
-		// TODO Auto-generated method stub
-		return null;
+		return timerCall.getAttribute("label");
 	}
-
 
 	@Override
 	public void clickCallTab() {
-		// TODO Auto-generated method stub
-		
+		Dimension dim = webview.getSize();
+		callTab.touchWithCoordinates(dim.width / 4 * 2 + 10, dim.height - 10);
 	}
-
 
 	@Override
 	public void clickEdit() {
-		// TODO Auto-generated method stub
-		
+		Rectangle point = arrowButton.getLocation();
+		switch (Devices.valueOf(DEVICE)) {
+		case IPHONE:
+		 arrowButton.touchWithCoordinates(point.getX(), point.getY());
+		 break;
+		case IOS7:
+		 arrowButton.touchWithCoordinates(point.getX(), point.getY() - 35);
+		 break;
+		 default:
+			 break;
+		}
 	}
 
+	public void clickConfirmation() {
+		Rectangle point = trashButton.getLocation();
+		trashButton.touchWithCoordinates(point.getX(), point.getY() - 35);
+	}
+
+	public void clickTrash() {
+		Rectangle point = trashButton.getLocation();
+		trashButton.touchWithCoordinates(point.getX(), point.getY());
+
+	}
 
 	@Override
 	public void findDeleteContacts() {
-		// TODO Auto-generated method stub
-		
-	}
+		Rectangle point = contact.getLocation();
 
+	}
 
 	@Override
 	public int deleteCall() {
-		// TODO Auto-generated method stub
-		return 0;
+		int count = getCountUsers();
+		clickTrash();
+		clickConfirmation();
+		return count;
 	}
-
 
 	@Override
 	public void deleteAllCalls() {
-		// TODO Auto-generated method stub
-		
+		Rectangle point = allContacts.getLocation();
+		allContacts.touchWithCoordinates(point.getX(), point.getY());
+		clickConfirmation();
 	}
-
 
 	@Override
 	public int getCountUsers() {
-		// TODO Auto-generated method stub
-		return 0;
+		List<WebElement> elements = ((AppiumDriver) driver).getDriver()
+				.findElements(By.xpath(FIRST_RESULT));
+		return elements.size();
 	}
-
 
 	@Override
 	public String getMessageEmptyList() {
-		// TODO Auto-generated method stub
-		return null;
+		return messageEmptyList.getAttribute("name");
 	}
-
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public HistoryFilterPageIos openFilter() {
+		Rectangle point = filterButton.getLocation();
+		filterButton.touchWithCoordinates(point.getX(), point.getY());
+		return PageFactory.initElements(driver, HistoryFilterPageIos.class);
+	}
 
 }
